8fc68de89d6b53ecab473516f43fca099b639a71
mewoodland <mewoodland@mdanderson.org>
2023-12-26 08:43:54 -0600

    add label base code - need to debug

--- utils/preprocess_CHAOS.py
+++ utils/preprocess_CHAOS.py
@@ -3,9 +3,13 @@ Prepares the CHAOS dataset for training.
 """
 
 # Imports
+import argparse
 import dicom2nifti
 import os
-import argparse
+import nibabel as nib
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
 
 # Arguments
 parser = argparse.ArgumentParser()
@@ -21,9 +25,29 @@ if not os.path.exists(os.path.join(base_dir, 'imagesTr')):
 if not os.path.exists(os.path.join(base_dir, 'labelsTr')):
   os.mkdir(os.path.join(base_dir, 'labelsTr'))
 
+# Functions
+def png_to_nifti(folder):
+  imgs = []
+  for img_name in sorted(os.listdir(folder)):
+    # Read in PNG image.
+    img_pil = Image.open(os.path.join(folder, img_name))
+    # Convert from PIL to numpy.
+    img_np = np.array(img_pil)
+    # Add a new first axis with dimension 1 to stack slices later.
+    img_np_exp = img_np[np.newaxis, :]
+    imgs.append(img_np_exp)
+  # Stack the slices into an image.
+  img_np = np.vstack(imgs)
+  # Rotate 90 degrees for NIfTI conversion.
+  img_np = np.rot90(img_np)
+  # Convert to a NIfTI image.
+  img_nii = nib.Nifti1Image(img_np, np.eye(4))
+  return img_nii
+
 # Main code
-for patient_id in os.listdir(os.path.join(base_dir, 'Train_Sets', 'MR')):
+for patient_id in tqdm(os.listdir(os.path.join(base_dir, 'Train_Sets', 'MR'))):
+  ground = png_to_nifti(os.path.join(base_dir, 'Train_Sets', 'MR', patient_id, 'T1DUAL', 'Ground'))
+  nib.save(ground, os.path.join(base_dir, 'labelsTr', f'MR_{patient_id}_T1DUAL_Ground.nii.gz'))
   for phase in os.listdir(os.path.join(base_dir, 'Train_Sets', 'MR', patient_id, 'T1DUAL', 'DICOM_anon')):
     dicom2nifti.dicom_series_to_nifti(os.path.join(base_dir, 'Train_Sets', 'MR', patient_id, 'T1DUAL', 'DICOM_anon', phase),
                                       os.path.join(base_dir, 'imagesTr', f'MR_{patient_id}_T1DUAL_DICOM_anon_{phase}.nii.gz'))
-  
